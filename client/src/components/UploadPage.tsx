84997f3f617f031f546279b44eab85dec9dab1b2
dstlmike <dstl_mike1@hotmail.com>
2021-02-15 21:57:21 -0500

    Update UploadPage.tsx

--- client/src/components/UploadPage.tsx
+++ client/src/components/UploadPage.tsx
@@ -37,7 +37,7 @@ return alert('Hi');
     }
 */
     fetchRecent = () => {
-        axios.get('http://localhost:8080/recent')
+        axios.get('http://localhost:9890/recent')
             .then((response) => {
                 this.setState({ recentImage: response.data.image });
             })
@@ -53,7 +53,7 @@ return alert('Hi');
         formData.append('caption', this.state.caption);
         formData.append('file', this.state.uploadedImage);
 
-        axios.post('http://localhost:8080/', formData)
+        axios.post('http://localhost:9890/', formData)
             .then((response) => {
                 response.data.success ? alert('File successfully uploaded') : alert('File already exists');
                // this.fetchRecent();
