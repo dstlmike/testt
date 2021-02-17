f20e3e1aae650391e73cbf63297e0f269e7db7b4
dstlmike <dstl_mike1@hotmail.com>
2021-02-16 19:40:39 -0500

    Update app.js

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ const imageRouter = require('./routes/image');
 const app = express();
 
 // view engine setup
-app.set('views', [path.join(__dirname + 'server/views'), path.join(__dirname + 'client/views')]);
+app.set('views', ['./server/views', './cliend/views']); // [__dirname + '/server/views'), path.join(__dirname + 'client/views')]);
 //app.use('views', path.join(__dirname, 'client/views'));
 app.use('view engine', 'jade');
 
