fc00523e28a838d63c114f38e50d875c397c6901
dstlmike <dstl_mike1@hotmail.com>
2021-02-20 00:38:12 -0500

    Update UploadPage.tsx

--- client/src/components/UploadPage.tsx
+++ client/src/components/UploadPage.tsx
@@ -38,7 +38,7 @@ return alert('Hi');
   //  }
 
     fetchRecent = () => {
-        axios.get('http://localhost:8080/recent')
+        axios.get('http://localhost:9890/recent')
             .then((response) => {
                 this.setState({ recentImage: response.data.image });
             })
@@ -54,7 +54,7 @@ return alert('Hi');
         formData.append('caption', this.state.caption);
         formData.append('file', this.state.uploadedImage);
 
-        axios.post('http://localhost:8080/', formData)
+        axios.post('http://localhost:9890/', formData)
             .then((response) => {
                 response.data.success ? alert('File successfully uploaded') : alert('File already exists');
                 this.fetchRecent();
@@ -74,7 +74,7 @@ return alert('Hi');
                         </div>
 
                       //  <img
-                        //    src={'http://localhost:8080/image/' + this.state.recentImage.filename}
+                        //    src={'http://localhost:9890/image/' + this.state.recentImage.filename}
                         //    alt="recent-image"
                         //    className="Recent__Image"
                      //   />
