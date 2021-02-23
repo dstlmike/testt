3e1115829a6ce445b4cb0384ec3436f9e305f23d
dstlmike <dstl_mike1@hotmail.com>
2021-02-23 01:06:43 -0500

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import '../src/index.css';
+import App from '../src/App';
+import * as serviceWorker from '../src/serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
